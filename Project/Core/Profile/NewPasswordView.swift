2a840b37b6c7de488a5550720004c23bf1694259
gursewaktrash <gursewaktrash@gmail.com>
2024-03-22 19:31:12 +0800

    Password Reset Alerts
    Have pop up message if password has been reset

--- Project/Core/Profile/NewPasswordView.swift
+++ Project/Core/Profile/NewPasswordView.swift
@@ -6,155 +6,116 @@
 //
 
 import SwiftUI
+import FirebaseAuth
 
 struct NewPasswordView: View {
     @State private var currentPassword: String = ""
-    @State private var newPassword: String = ""
-    @State private var confirmPassword: String = ""
-    @State private var errorMessage: String = ""
-    @State private var successMessage: String = ""
-    
-    @EnvironmentObject var viewModel: AuthViewModel
-    
-    var body: some View {
-        NavigationView {
-                   VStack {
-                       //Header
-                       HStack{
-                           ZStack{
-                               RoundedRectangle(cornerRadius: 10)
-                                   .foregroundColor(.clear)
-                                   .frame(width: 40, height: 40)
-                                   .overlay(
-                                           RoundedRectangle(cornerRadius: 10)
-                                               .stroke(Color.black, lineWidth: 1)
-                                       )
-                               
-                               NavigationLink(
-                                   destination: ProfileView().navigationBarHidden(true),
-                                   label: {
-                                       Image(systemName: "chevron.backward")
-                                           .font(Font.custom("Alatsi", size: 15))
-                                           .foregroundColor(Color(red: 0.08, green: 0.13, blue: 0.30))
-                                   })
-                           }
-                           .offset(x: -45, y: 0)
-                           
-                           Text("Change Password")
-                               .font(Font.custom("Alatsi", size: 25))
-                               .foregroundColor(Color(red: 0.078, green: 0.13, blue: 0.30))
-                               .padding()
-                               .offset(x: -30, y: 0)
-                          
-                       }
-                       .offset(x: 0, y: -40)
-                      
-                       //Image
-                       ZStack{
-                           Image("restPass")
-                               .resizable()
-                               .frame(width: 130, height: 130)
-                           
-                       }.padding(.bottom,40)
-                       
-                       Text("Current Password")
-                           .font(Font.custom("Alatsi", size: 15))
-                           .foregroundColor(Color(red: 0.45, green: 0.45, blue: 0.45))
-                           .offset(x: -108.50, y: 0)
-                       
-                       ZStack {
-                           Rectangle()
-                               .foregroundColor(.clear)
-                               .frame(width: 307, height: 38)
-                               .background(Color(red: 0.93, green: 0.93, blue: 0.93))
-                               .cornerRadius(5)
-                           
-                           SecureField("Enter Your Current Password", text: $currentPassword)
-                               .offset(x: 65, y: 0)
-                       }
-                       .padding(.bottom)
-                       
-                       Text("New Password")
-                           .font(Font.custom("Alatsi", size: 15))
-                           .foregroundColor(Color(red: 0.45, green: 0.45, blue: 0.45))
-                           .offset(x: -113.50, y: 0)
-                       
-                       ZStack {
-                           Rectangle()
-                               .foregroundColor(.clear)
-                               .frame(width: 307, height: 38)
-                               .background(Color(red: 0.93, green: 0.93, blue: 0.93))
-                               .cornerRadius(5)
-                           
-                           SecureField("Enter Your New Password", text: $newPassword)
-                               .offset(x: 65, y: 0)
-                       }
-                       .padding(.bottom)
-                       
-                       Text("Confirm New Password")
-                           .font(Font.custom("Alatsi", size: 15))
-                           .foregroundColor(Color(red: 0.45, green: 0.45, blue: 0.45))
-                           .offset(x: -86, y: 0)
-                       
-                       ZStack {
-                           Rectangle()
-                               .foregroundColor(.clear)
-                               .frame(width: 307, height: 38)
-                               .background(Color(red: 0.93, green: 0.93, blue: 0.93))
-                               .cornerRadius(5)
-                           
-                           SecureField("Enter Your Password Again", text: $confirmPassword)
-                               .offset(x: 65, y: 0)
-                       }
-                       
-                       
-                       
-                       // Rest Button
-                       ZStack {
-                           Rectangle()
-                               .foregroundColor(.clear)
-                               .frame(width: 228, height: 38)
-                               .background(Color(red: 0.66, green: 0.13, blue: 0.16))
-                               .cornerRadius(5)
-                               .offset(x: 0, y: 40)
-                           
-                           Button("Submit") {
-                               Task {
-                                   do {
-                                       try await viewModel.updatePassword(newPassword: newPassword, currentPassword: currentPassword) { error in
-                                           if let error = error {
-                                               print("Failed to update password with error: \(error.localizedDescription)")
-                                           } else {
-                                               print("Password updated successfully!")
-                                           }
-                                       }
-                                   } catch {
-                                       print("Error: \(error.localizedDescription)")
-                                   }
-                               }
-                           }
-                           .font(Font.custom("Alatsi", size: 18))
-                            .foregroundColor(.white)
-                            .padding(.top, 80)
-                       }
-                       
-                       Group{
-                           if !errorMessage.isEmpty {
-                               Text(errorMessage)
-                                   .foregroundColor(.red)
-                                   .padding()
-                           }
-                           
-                           if !successMessage.isEmpty {
-                               Text(successMessage)
-                                   .foregroundColor(.green)
-                                   .padding()
-                           }
-                       }
-                
-                   }
-               }
-           }
+        @State private var newPassword: String = ""
+        @State private var confirmPassword: String = ""
+        @State private var alertMessage: String = ""
+        @State private var showAlert = false
+        
+        @AppStorage("isUserAuthenticated") private var isUserAuthenticated = false
+        @EnvironmentObject var viewModel: AuthViewModel
+        
+        var body: some View {
+            NavigationView {
+                VStack(spacing: 40) {
+                    HStack {
+                        Button(action: {
+                            // Handle back navigation
+                        }) {
+                            Image(systemName: "chevron.backward")
+                                .font(Font.custom("Alatsi", size: 15))
+                                .foregroundColor(Color(red: 0.08, green: 0.13, blue: 0.30))
+                        }
+                        .frame(width: 40, height: 40)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.black, lineWidth: 1)
+                        )
+                        .padding(.leading, -45)
+                        
+                        Text("Change Password")
+                            .font(Font.custom("Alatsi", size: 25))
+                            .foregroundColor(Color(red: 0.078, green: 0.13, blue: 0.30))
+                    }
+                    
+                    Image("restPass")
+                        .resizable()
+                        .frame(width: 130, height: 130)
+                        .padding(.bottom, 40)
+                    
+                    VStack(alignment: .leading, spacing: 0) {
+                        Text("Current Password")
+                            .font(Font.custom("Alatsi", size: 15))
+                            .foregroundColor(Color(red: 0.45, green: 0.45, blue: 0.45))
+                        
+                        SecureField("Enter Your Current Password", text: $currentPassword)
+                            .frame(height: 38)
+                            .background(Color(red: 0.93, green: 0.93, blue: 0.93))
+                            .cornerRadius(5)
+                    }
+                    
+                    VStack(alignment: .leading, spacing: 0) {
+                        Text("New Password")
+                            .font(Font.custom("Alatsi", size: 15))
+                            .foregroundColor(Color(red: 0.45, green: 0.45, blue: 0.45))
+                        
+                        SecureField("Enter Your New Password", text: $newPassword)
+                            .frame(height: 38)
+                            .background(Color(red: 0.93, green: 0.93, blue: 0.93))
+                            .cornerRadius(5)
+                    }
+                    
+                    VStack(alignment: .leading, spacing: 0) {
+                        Text("Confirm New Password")
+                            .font(Font.custom("Alatsi", size: 15))
+                            .foregroundColor(Color(red: 0.45, green: 0.45, blue: 0.45))
+                        
+                        SecureField("Enter Your Password Again", text: $confirmPassword)
+                            .frame(height: 38)
+                            .background(Color(red: 0.93, green: 0.93, blue: 0.93))
+                            .cornerRadius(5)
+                    }
+                    
+                    Button("Submit") {
+                        Task {
+                            do {
+                                try await viewModel.updatePassword(newPassword: newPassword, currentPassword: currentPassword) { error in
+                                    if let error = error {
+                                        alertMessage = "Failed to update password: \(error.localizedDescription)"
+                                    } else {
+                                        alertMessage = "Password updated successfully!"
+                                        try? Auth.auth().signOut()
+                                        isUserAuthenticated = false
+                                    }
+                                    showAlert = true
+                                }
+                            } catch {
+                                alertMessage = "Error: \(error.localizedDescription)"
+                                showAlert = true
+                            }
+                        }
+                    }
+                    .font(Font.custom("Alatsi", size: 18))
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color(red: 0.66, green: 0.13, blue: 0.16))
+                    .cornerRadius(5)
+                    
+                    Group {
+                        if !alertMessage.isEmpty {
+                            Text(alertMessage)
+                                .foregroundColor(alertMessage == "Password updated successfully!" ? .green : .red)
+                                .padding()
+                        }
+                    }
+                    
+                }
+                .padding()
+            }
+        }
     }
 
 #Preview {
