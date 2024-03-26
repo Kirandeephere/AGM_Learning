b1fe852fbf77fc53fdd10d8655d36157655bafee
gursewaktrash <gursewaktrash@gmail.com>
2024-03-26 19:32:21 +0800

    Added ShowCalender
    Added ShowCalender

--- Project/Core/Booking/BookingView.swift
+++ Project/Core/Booking/BookingView.swift
@@ -8,11 +8,21 @@
 import SwiftUI
 import Firebase
 
+extension TimeInterval {
+    func minutesToHoursMinutes() -> String {
+        let formatter = DateComponentsFormatter()
+        formatter.unitsStyle = .positional
+        formatter.zeroFormattingBehavior = .pad
+        formatter.allowedUnits = [.hour, .minute]
+        return formatter.string(from: self) ?? ""
+    }
+}
+
 struct BookingView: View {
     let helpTypes = ["Homework", "Study", "Project", "Exam", "Other"]
     let name: String
     let subject: String
-
+    
     @State private var description: String = ""
     @State private var selectedHelpType: String = ""
     @State private var otherHelpType: String = ""
@@ -21,102 +31,103 @@ struct BookingView: View {
     @State private var selectedDuration: TimeInterval = 0
     @State private var isShowingBookCompletionAlert = false
     @State private var isNavigatingToHomeView = false
+    
     @EnvironmentObject var viewModel: AuthViewModel
-
+    
     var user: User? {
         viewModel.currentUser
     }
     
-    
     var nextDate: Date {
-            let calendar = Calendar.current
-            return calendar.date(byAdding: .day, value: 1, to: Date()) ?? Date()
-        }
+        let calendar = Calendar.current
+        return calendar.date(byAdding: .day, value: 1, to: Date()) ?? Date()
+    }
     
     private func saveBookingToFirebase() {
-        if let user = viewModel.currentUser {
-            let userBookingsRef = Database.database().reference().child("Bookings").child(user.fullname)
-            let bookingID = userBookingsRef.childByAutoId().key ?? ""
-            
-            let dateFormatter = DateFormatter()
-            dateFormatter.dateFormat = "yyyy-MM-dd"
-            let dateString = dateFormatter.string(from: selectedDate)
-            
-            let timeFormatter = DateFormatter()
-            timeFormatter.dateFormat = "HH:mm"
-            let timeString = timeFormatter.string(from: selectedTime)
-            
-            let durationFormatter = DateComponentsFormatter()
-            durationFormatter.unitsStyle = .full
-            durationFormatter.allowedUnits = [.hour, .minute]
-            let durationString = durationFormatter.string(from: selectedDuration)
-            
-            let bookingDetails: [String: Any] = [
-                "name": name,
-                "subject": subject,
-                "description": description,
-                "helpType": selectedHelpType,
-                "otherHelpType": otherHelpType,
-                "date": dateString,
-                "time": timeString,
-                "duration": durationString ?? ""
-            ]
-
-            let childUpdates = ["/\(bookingID)": bookingDetails]
-            userBookingsRef.updateChildValues(childUpdates) { error, _ in
-                if let error = error {
-                    print("Error saving booking to Firebase: \(error)")
-                } else {
-                    print("Booking saved successfully to Firebase")
-                }
+        guard let user = viewModel.currentUser else { return }
+        
+        let userBookingsRef = Database.database().reference().child("Bookings").child(user.fullname)
+        let bookingID = userBookingsRef.childByAutoId().key ?? ""
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let dateString = dateFormatter.string(from: selectedDate)
+        
+        let timeFormatter = DateFormatter()
+        timeFormatter.dateFormat = "HH:mm"
+        let timeString = timeFormatter.string(from: selectedTime)
+        
+        let durationFormatter = DateComponentsFormatter()
+        durationFormatter.unitsStyle = .full
+        durationFormatter.allowedUnits = [.hour, .minute]
+        let durationString = durationFormatter.string(from: selectedDuration)
+        
+        let bookingDetails: [String: Any] = [
+            "name": name,
+            "subject": subject,
+            "description": description,
+            "helpType": selectedHelpType,
+            "otherHelpType": otherHelpType,
+            "date": dateString,
+            "time": timeString,
+            "duration": durationString ?? ""
+        ]
+        
+        let childUpdates = ["/\(bookingID)": bookingDetails]
+        userBookingsRef.updateChildValues(childUpdates) { error, _ in
+            if let error = error {
+                print("Error saving booking to Firebase: \(error)")
+            } else {
+                print("Booking saved successfully to Firebase")
             }
         }
     }
     
     var body: some View {
-        VStack(spacing: 20) {
+        VStack(spacing: 10) {
             Text("Booking Details")
                 .font(.largeTitle)
                 .padding()
+            
+            ScrollView {
+                VStack(alignment: .leading, spacing: 10) {
+                    bookingDetail(title: "Name", value: name)
+                    bookingDetail(title: "Subject", value: subject)
+                    
+                    Spacer()
 
-            ScrollView{
-                VStack(alignment: .leading){
-                    VStack(alignment: .leading, spacing: 10) {
-                        Text("Name")
-                            .font(.headline)
-                        Text(name)
-                            .font(.subheadline)
-                            .foregroundColor(.gray)
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .padding()
-                            .background(Color(.systemGray5))
-                            .cornerRadius(8)
-                    }
-                    
-                    VStack(alignment: .leading, spacing: 10) {
-                        Text("Subject")
-                            .font(.headline)
-                        Text(subject)
-                            .font(.subheadline)
-                            .foregroundColor(.gray)
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .padding()
-                            .background(Color(.systemGray5))
-                            .cornerRadius(8)
-                    }
-                    
+                    // Description Feild
                     TextField("Description", text: $description)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .textFieldStyle(.plain)
                         .padding()
-                    
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                        .frame(maxWidth: 350, minHeight: 50)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 8)
+                                .stroke(Color.gray, lineWidth: 1)
+                        )
+                        .padding(.horizontal, 10)
+                        .offset(x: 10)
+
+                    Spacer()
+
+                    // Dropdown for Seclection
                     Picker("Help Type", selection: $selectedHelpType) {
                         ForEach(helpTypes, id: \.self) { helpType in
                             Text(helpType)
                                 .font(.headline)
                         }
                     }
-                    .pickerStyle(.menu)
-                    .padding()
+                    .pickerStyle(MenuPickerStyle())
+                    .frame(maxWidth: 350, minHeight: 50)
+                    .padding(.horizontal, 20)
+                    .background(Color(.systemGray5))
+                    .cornerRadius(8)
+                    .padding(.horizontal, 20)
+                    .foregroundColor(.gray)
+
+                    Spacer()
                     
                     if selectedHelpType == "Other" {
                         TextField("Enter Help Type", text: $otherHelpType)
@@ -124,93 +135,117 @@ struct BookingView: View {
                             .padding()
                     }
                     
-                    HStack {
-                        Text("Day") // Custom label
-                            .font(.headline)
-                        
-                        DatePicker("", selection: $selectedDate,in: nextDate..., displayedComponents: .date)
-                            .labelsHidden()
-                            .datePickerStyle(CompactDatePickerStyle())
-                        
-                        
-                    }
                     
+                    //Date Time and Duration Stack
                     HStack {
-                        Text("Time: ") // Custom label
-                            .font(.headline)
+                        VStack {
+                            dateTimePicker(title: "Day", date: $selectedDate, components: .date)
+                                .fixedSize()
+
+                            dateTimePicker(title: "Time", date: $selectedTime, components: .hourAndMinute)
+                                .fixedSize()
+
+                        }
+                        .padding()
+                        .background(
+                            RoundedRectangle(cornerRadius: 8)
+                                .foregroundColor(Color(.systemGray6))
+                        )
                         
-                        DatePicker("", selection: $selectedTime,displayedComponents: .hourAndMinute)
-                            .labelsHidden()
-                            .datePickerStyle(CompactDatePickerStyle())
                         
-                    }
-                    
-                    HStack {
-                        Text("Duration: ") // Custom label
-                            .font(.headline)
+                        VStack {
+                            Text("Duration")
+                                .font(.headline)
+                                .padding(.bottom, 4)
+                            
                         
-                        RoundedRectangle(cornerRadius: 8)
-                            .frame(width: 130, height: 40)
-                            .foregroundColor(Color(.systemGray6))
-                            .overlay(
-                                Picker("Select Duration", selection: $selectedDuration) {
-                                    ForEach(0..<20) { index in
-                                        let duration = TimeInterval(index) * 15 * 60
-                                        Text("\(duration.minutesToHoursMinutes())")
-                                            .tag(duration)
+                        Picker("Select Duration", selection: $selectedDuration) {
+                                        ForEach(0..<20) { index in
+                                            let duration = TimeInterval(index) * 15 * 60
+                                            Text("\(duration.minutesToHoursMinutes())")
+                                                .tag(duration)
+                                        }
                                     }
-                                }.accentColor(.black)
-                                    .pickerStyle(.menu)
-                            )
+                                    .fixedSize()
+                                    .pickerStyle(MenuPickerStyle())
+                                    .padding()
+                                    .background(
+                                        RoundedRectangle(cornerRadius: 8)
+                                            .foregroundColor(Color(.systemGray5))
+                                    )
+                        }
+                        .padding()
+                        .background(
+                        RoundedRectangle(cornerRadius: 8)
+                        .foregroundColor(Color(.systemGray6))
+                        )
+            
                         
                     }
+                    .padding()
                     
                     
-
-
-
-                            Button {
-                                isShowingBookCompletionAlert = true
-                                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                                    isShowingBookCompletionAlert = false
-                                    isNavigatingToHomeView = true
-                                    saveBookingToFirebase()
-                                }
-                            } label: {
-                                Text("Book")
-                                    .font(Font.custom("Alatsi", size: 20))
-                                    .foregroundColor(.white)
-                                    .padding()
-                                    .background(Color.blue)
-                                    .cornerRadius(10)
-                            }
+                    //Book Button
+                    Button(action: {
+                        isShowingBookCompletionAlert = true
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                            isShowingBookCompletionAlert = false
+                            isNavigatingToHomeView = true
+                            saveBookingToFirebase()
+                        }
+                    }) {
+                        Text("Book")
+                            .font(Font.custom("Alatsi", size: 20))
+                            .foregroundColor(.white)
                             .padding()
-                            .alert(isPresented: $isShowingBookCompletionAlert) {
-                                Alert(
-                                    title: Text("Book Completed!"),
-                                    message: Text("Congratulations on completing the book!")
-                                )
-                                
-                            }
-                            .background(
-                                NavigationLink(destination: HomeView().navigationBarBackButtonHidden(true), isActive: $isNavigatingToHomeView) { EmptyView() }
-                            )
-                        
-                    
-                    
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                    }
+                    .offset(x: 140)
+                    .padding()
+                    .alert(isPresented: $isShowingBookCompletionAlert) {
+                        Alert(
+                            title: Text("Book Completed!"),
+                            message: Text("Congratulations on completing the book!")
+                        )
+                    }
+                    .background(
+                        NavigationLink(destination: HomeView().navigationBarBackButtonHidden(true), isActive: $isNavigatingToHomeView) {
+                            EmptyView()
+                        }
+                    )
                 }
+                .padding()
             }
-
         }
-        .padding()
     }
-}
+    
+    private func bookingDetail(title: String, value: String) -> some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Text(title)
+                .font(.headline)
+                .padding(.horizontal, 20)
 
-extension TimeInterval {
-    func minutesToHoursMinutes() -> String {
-        let hours = Int(self) / 3600
-        let minutes = Int(self) % 3600 / 60
-        return String(format: "%02d:%02d hr", hours, minutes)
+            
+            Text(value)
+                .font(.subheadline)
+                .foregroundColor(.gray)
+                .frame(width: 350, height: 50)
+                .background(Color(.systemGray5))
+                .cornerRadius(8)
+                .padding(.horizontal, 20)
+        }
+    }
+    
+    private func dateTimePicker(title: String, date: Binding<Date>, components: DatePickerComponents) -> some View {
+        HStack {
+            Text("\(title): ") // Custom label
+                .font(.headline)
+            
+            DatePicker("", selection: date, displayedComponents: components)
+                .labelsHidden()
+                .datePickerStyle(CompactDatePickerStyle())
+        }
     }
 }
 
--- Project/Core/Booking/CalenderView.swift
+++ Project/Core/Booking/CalenderView.swift
@@ -6,6 +6,9 @@
 //
 
 import SwiftUI
+import FirebaseFirestore
+
+import SwiftUI
 import Firebase
 
 
@@ -15,21 +18,66 @@ struct CalendarView: View {
     @State private var isShowingBookingView = false
     
     var body: some View {
-        VStack {
-            Text("Upcoming Bookings")
-                .font(.headline)
             
-            Button(action: {
-                isShowingBookingView = true
-            }) {
-                Text("View")
+        NavigationView {
+            VStack {
+                ZStack {
+                    HStack(spacing: 0) {
+                        VStack() {
+                            Image("calendar")
+                                .font(Font.custom("Alatsi", size: 25))
+                                .foregroundColor(Color(red: 0.08, green: 0.13, blue: 0.30))
+                                .offset(x: 30, y: -12)
+                        }
+                        .frame(width: 74, height: 74)
+                    }
+                    .frame(width: 74, height: 74)
+                    .offset(x: -150, y: 12)
+                    
+                    Text("Calendar")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .offset(x: -20, y: -20)
+                    
+                    NavigationLink(destination: MyBookingView()) {
+                        Text("Check all upcoming appointments >")
+                            .font(Font.custom("Alatsi", size: 15).bold())
+                            .foregroundColor(Color(red: 0.66, green: 0.13, blue: 0.16))
+                            .offset(x: 40, y: 16.50)
+                    }
+                }
+                .frame(width: 319, height: 74)
+                .padding(.top, 30)
+                .padding(.bottom, 40)
+                
+
+                ShowCalendar()
+
+                Spacer()
             }
-            .sheet(isPresented: $isShowingBookingView) {
-                MyBookingView()
-            }
+
         }
+        
+        //Kirandeep Code -
+//        VStack {
+//            Text("Upcoming Bookings")
+//                .font(.headline)
+//            
+//            Button(action: {
+//                isShowingBookingView = true
+//            }) {
+//                Text("View")
+//            }
+//            .sheet(isPresented: $isShowingBookingView) {
+//                MyBookingView()
+//            }
+//        }
     }
 }
+
+//Show Calender Table
+
+
 
 #Preview {
     CalendarView()
